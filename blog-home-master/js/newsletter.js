dd4295bed7de6572f5443d13c92dd9b644f82f1f
ppoulo <925420636@qq.com>
2024-11-28 22:39:02 +0800

    first commit

--- blog-home-master/js/newsletter.js
+++
@@ -1,9 +0,0 @@
-var esubmit= document.getElementById('submit');
-const 文字栏 = document.querySelector(".文字栏");
-
-
-esubmit.onclick=function(){
-    alert("Message has been successfully sent!")
-    文字栏.value="";
-}
-
